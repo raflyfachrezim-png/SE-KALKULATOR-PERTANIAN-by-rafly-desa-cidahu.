2c33752b7fbb78493cae3c930e863116108f5b4e
raflyfachrezim-png <raflyfachrezim@gmail.com>
2026-07-04 10:28:09 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,63 @@
+const database = [
 
+{
+    nama:"Padi",
+    luas:1000,
+    pegawai:2,
+    hariKerja:7,
+    upah:840000,
+    produksi:700000,
+    operasional:200000,
+    nonOperasional:0,
+    hasilPanen:600,
+    harga:5000,
+    pendapatan:3000000,
+    panen:3
+},
+
+{
+    nama:"Sawi",
+    luas:1000,
+    pegawai:2,
+    hariKerja:7,
+    upah:840000,
+    produksi:900000,
+    operasional:450000,
+    nonOperasional:0,
+    hasilPanen:900,
+    harga:3000,
+    pendapatan:2700000,
+    panen:12
+},
+
+{
+    nama:"Timun",
+    luas:1000,
+    pegawai:5,
+    hariKerja:10,
+    upah:3000000,
+    produksi:7060000,
+    operasional:1500000,
+    nonOperasional:0,
+    hasilPanen:3000,
+    harga:4000,
+    pendapatan:12000000,
+    panen:10
+},
+
+{
+    nama:"Cabe",
+    luas:1000,
+    pegawai:5,
+    hariKerja:10,
+    upah:3000000,
+    produksi:23000000,
+    operasional:5000000,
+    nonOperasional:0,
+    hasilPanen:1250,
+    harga:30000,
+    pendapatan:37500000,
+    panen:2
+}
+
+];
